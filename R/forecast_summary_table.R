5dd90b07fb6fc89978fe9b453f61504cd2adc285
Greg Buck <gregory.buck@alaska.gov>
2020-10-29 16:18:23 -0800

    update PE

--- R/forecast_summary_table.R
+++ R/forecast_summary_table.R
@@ -165,7 +165,7 @@ f.cast.tbl <- function(f.cast.age,sibling,f.cast.yr,riv.name,start.yr,model){
   temp <- master %>%
     filter(return.year > f.cast.yr - 10) %>%
     mutate(AD = abs(forecast - f.cast.age)) %>%
-    mutate(PE = abs(forecast - f.cast.age)/f.cast.age) %>%
+    mutate(PE = (forecast - f.cast.age)/f.cast.age) %>%
     mutate(APE = abs(PE)) %>%
     mutate(AAPE = atan(abs((f.cast.age - forecast)/f.cast.age))) %>%
     mutate(brood.year = as.character(brood.year)) %>%
